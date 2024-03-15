0785b26dc56e0cbc0eb35d7c7fdc164ccb37a590
ricardcorrales <you@example.com>
2024-03-15 12:49:02 +0100

    nuevo commit

--- ToDoItem.cs
+++ ToDoItem.cs
@@ -3,8 +3,9 @@ namespace BlazorApp
     public class ToDoItem
     {
         public string? Title {get; set;}
-
         public bool? IsDown {get; set;}
-
+        public ToDoItem()
+        {       
+        }
     }
 }
